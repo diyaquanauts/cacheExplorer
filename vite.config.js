e3068a840e5dd063f33c393b49964b40cdd7d8f5
Brett <brettmsmith@protonmail.com>
2024-08-18 04:10:39 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cacheExplorer/', // Set the base path to the repo name
 })
